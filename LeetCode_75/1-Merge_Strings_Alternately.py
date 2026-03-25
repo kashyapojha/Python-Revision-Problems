097ae2a3bbc7deecd6bded8d9aafad4434b52010
Kashyap Ojha <kashyap.ojha24@gmail.com>
2026-03-25 18:40:51 +0530

    LeetCode P-1

--- LeetCode_75/1-Merge_Strings_Alternately.py
+++ LeetCode_75/1-Merge_Strings_Alternately.py
@@ -4,22 +4,23 @@ Merge the strings by adding letters in alternating order, starting with word1.
 If a string is longer than the other, append the additional letters onto the end of the merged string.
 Return the merged string.
 '''
-word1 = str(input("Enter the word-1:-"))
-word2 = str(input("Enter the word-2:-"))
-i = 0  
+
+
+word1 = input("Enter the word1:-")
+word2 = input("Enter the word2:-")
+
+i = 0
 j = 0
 result = ""
-while i < len(word1) and i < len(word2):
+while i < len(word1) and j < len(word2):
     result += word1[i]
-    result += word2[i]
+    result += word2[j]
     i += 1
     j += 1
 
 result += word1[i:]
 result += word2[j:]
 
-print("Merged string is " , result)
+print("Merged string is", result)
 
 
-
-    
