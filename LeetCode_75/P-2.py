9cac360ca9719a7cd6b06e72ccae667a75ad56cf
Kashyap Ojha <kashyap.ojha24@gmail.com>
2026-03-26 09:31:39 +0530

    Greatest Common Divisor of Strings

---
+++ LeetCode_75/P-2.py
@@ -0,0 +1,44 @@
+'''
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t 
+(i.e., t is concatenated with itself one or more times).
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+Example 1:
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+Example 4:
+Input: str1 = "AAAAAB", str2 = "AAA"
+Output: ""​​​​​​​
+Constraints:
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of English uppercase letters.
+'''
+
+from math import gcd
+str1= input("Enter a string-1")
+str2= input("Enter a string-2")
+if str1 + str2 != str2 + str1:
+        result = ""
+else:
+        length = gcd(len(str1), len(str2))
+        result = str1[:length]
+
+print("string x is :-", result)
+
+'''
+LeetCode format:- 
+
+from math import gcd
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        length = gcd(len(str1), len(str2))
+        return str1[:length]
+
+'''
